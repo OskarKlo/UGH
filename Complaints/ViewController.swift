7f1904f0db393d817c7fce9fdf2d2a752713a8f1
OskarKlo <oskark@thinkglobalschool.com>
2017-11-05 10:17:48 +0530

    Table View Controller added and the realtime database is in progress, sorting out data read and auth

--- Complaints/ViewController.swift
+++ Complaints/ViewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import Firebase
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var ughTitleLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var complainTextField: UITextField!
-    @IBOutlet weak var submitComplaintButton: UIButton!
+    @IBOutlet weak var userComplainTextField: UITextField!
+    @IBOutlet weak var submitComplainButton: UIButton!
+    
+    //let ref = FIRDatabase.database().reference(withPath: "complaints")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    @IBAction func submitUghButton(_ sender: UIButton) {
-        if let text = complainTextField.text, !text.isEmpty
+    
+    @IBAction func submitComplainActionButton(_ sender: UIButton) {
+        if let text = userComplainTextField.text, !text.isEmpty
         {
             //do something if it's not empty
             performSegue(withIdentifier: "complaintSubmitSegue", sender: nil)
